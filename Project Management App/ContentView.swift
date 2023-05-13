a259358f20f18f80a20827241b70a9e709e02dc7
GeraudLuku <lukugeraud97@gmail.com>
2023-05-13 18:15:55 +0100

    Initial code

--- Project Management App/ContentView.swift
+++ Project Management App/ContentView.swift
@@ -9,50 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            VStack {
-                TabView {
-                    HomeScreen()
-                        .tabItem {
-                            Image(systemName: "house.fill")
-                        }
-                        .toolbarBackground(Color.white, for: .tabBar)
-
-                    Text("The content")
-                        .tabItem {
-                            Image(systemName: "folder.fill")
-                        }
-
-                    Text("The content")
-                        .tabItem {
-                            Image(systemName: "doc.plaintext.fill")
-                        }
-
-                    Text("The content")
-                        .tabItem {
-                            Image(systemName: "calendar")
-                        }
-                } // TAB VIEW
-                .accentColor(.black)
-            }
-            .toolbar {
-                
-                ToolbarItemGroup(placement: .navigationBarLeading) {
-                    
-                    CircularImageView(name: "profile", size: 42)
-                    
-                    Text("Hi, Kira!")
-                        .font(.footnote)
-                        .foregroundColor(.gray)
-                    
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Image(systemName: "bell.fill")
-                }
-                
-            }
-        }
+        
+        Text("Welcome to this tutorial")
+        
     }
 }
 
